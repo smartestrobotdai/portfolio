a39b8d6f955086e84dfe170ff93b3fccbd9bbc96
Ken <smartestrobotdai@gmai.com>
2022-10-04 06:26:36 +0200

    -

--- r-code/util.r
+++ r-code/util.r
@@ -3,15 +3,47 @@ library(dplyr)
 library(quantmod)
 library(data.table)
 library(stringr)
+library(TTR)
 
 COURTAGE=0.00089
 
+myEMA <- function (price,n, beta){
+  ema <- c()
+  ema[1:(n-1)] <- NA
+  ema[n]<- mean(price[1:n])
+  for (i in (n+1):length(price)){
+    ema[i]<-beta * price[i] + 
+      (1-beta) * ema[i-1]
+  }
+  ema <- reclass(ema,price)
+  return(ema)
+}
 
+ema <- function(df, n, beta) {
+  # calculate ema
+  n <- ceiling(abs(n))
+  if (n == 0) {
+    n = 1
+  }
+  beta <- abs(beta)
+  if (beta > 1) {
+    beta = 1
+  }
+  df$mean <- (df$high + df$low)/2
+  origin_mean <- exp(1)**df$mean
+  myEma <- myEMA(origin_mean, n=n, beta=beta)
+  
+  myEma <- shift(myEma)
+  myEma[1:n+1] <- origin_mean[1:n+1]
+  myEma[1] <- origin_mean[1]
+  df <- df %>% mutate(predict=log(myEma))
+  return(df)
+}
 
 kalman <- function(df, HHt_val, GGt_val) {
   df$mean <- (df$high + df$low)/2
   # kalman filter should accept the original number instead of log.
-  y <- as.numeric((exp(1)**df$mean)/10)
+  y <- as.numeric((exp(1)**df$mean))
   dt <- ct <- matrix(0)
   Tt <- matrix(1) # nolint
   a0 <- y[1] # Estimation of the first year flow
@@ -28,21 +60,30 @@ kalman <- function(df, HHt_val, GGt_val) {
 
   len <- length(y)
   #df <- data.frame(y, res$at[1,1:len], resid[1:len], df$high, df$low, df$close, df$open, shift(df$close))
-  df <- df %>% mutate(predict=log(res$at[1,1:len]*10), last_close=shift(df$close))
+  df <- df %>% mutate(predict=log(res$at[1,1:len]))
 
   return(df)
 }
 
-get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, same_day_trade_allowed=TRUE, courtage=COURTAGE) {
+get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, 
+  type='kalman',
+  same_day_trade_allowed=TRUE, 
+  courtage=COURTAGE) {
     log_buy_deviation <- par[1]
     log_sell_deviation <- par[2]
-    HHt_val <- par[3]
-    GGt_val <- par[4]
-    if (par[5]>=1 || par[5] <=0 ) {
+    if (type=='kalman') {
+      HHt_val <- par[3]
+      GGt_val <- par[4]
+    } else {
+      days <- par[3]
+      beta <- par[4]
+    }
+    stop_loss <- par[5]
+    if (stop_loss>=1 || stop_loss <=0 ) {
       if (verbose) print(str_glue("warning stop:loss {par[5]} is invalid"))
       return(NULL)
     }
-    log_stop_loss <- log(par[5])  # always negative
+    log_stop_loss <- log(stop_loss)  # always negative
     hold <- 0
     courtage_log <- log(1 - courtage)  #default -0.0015011
     buy_point <- 0
@@ -56,6 +97,7 @@ get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, same_day_trade_a
       high <- x["high"]
       last_close <- x["last_close"]
       if(is.na(last_close)) {
+        print('last_close is na')
         last_close = open
       }
 
@@ -91,9 +133,9 @@ get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, same_day_trade_a
         trade_times <<- trade_times+1
         if (verbose) {
           if (price == low_stop_loss_threshold) {
-            print(str_glue("SELL(STOP): row_id {row_id} up:{up} low:{low} stop_loss:{low_stop_loss_threshold} trade_times:{trade_times}"))
+            print(str_glue("SELL(STOP): row_id {row_id} up:{up} profit:{price-buy_point} low:{low} stop_loss:{low_stop_loss_threshold} sell_price: {price} trade_times:{trade_times}"))
           } else {
-            print(str_glue("SELL: row_id {row_id} up:{up} high:{high} price:{price} sell:{high_sell_threshold} trade_times:{trade_times}"))
+            print(str_glue("SELL: row_id {row_id} up:{up} profit:{price-buy_point} high:{high} price:{price} sell_threshold:{high_sell_threshold} sell_price: {price} trade_times:{trade_times}"))
           }
         }
       }      
@@ -102,20 +144,24 @@ get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, same_day_trade_a
         # the max (low_buy_threshold, low_stop_loss_threshold) come first
         if (up) {
           if (low < low_stop_loss_threshold) {
-            sell_at(low_stop_loss_threshold)
-            profit <- low_stop_loss_threshold - last_close + courtage_log
+            sell_price = min(low_stop_loss_threshold, open)
+            sell_at(sell_price)
+            profit <- sell_price - last_close + courtage_log
           } else if(high > high_sell_threshold) {
-            sell_at(high_sell_threshold)
-            profit <- high_sell_threshold - last_close + courtage_log
-          } 
+            sell_price = max(open, high_sell_threshold)
+            sell_at(sell_price)
+            profit <- sell_price - last_close + courtage_log
+          } else {
+            profit <- close - last_close
+          }
         } else {
           if(high > high_sell_threshold) {
-            sell_at(high_sell_threshold)
-            profit <- high_sell_threshold - last_close + courtage_log
+            sell_price = max(open, high_sell_threshold)
+            sell_at(sell_price)
+            profit <- sell_price - last_close + courtage_log
             if (low < low_buy_threshold) {
               buy_at(low_buy_threshold)
               profit <- profit + courtage_log
-              low_stop_loss_threshold <- buy_point + log_stop_loss
               if (low < low_stop_loss_threshold) {
                 sell_at(low_stop_loss_threshold)
                 profit <- profit + low_stop_loss_threshold - buy_point + courtage_log
@@ -124,16 +170,19 @@ get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, same_day_trade_a
               }
             }
           } else if (low < low_stop_loss_threshold) {
-            sell_at(low_stop_loss_threshold)
-            profit <- low_stop_loss_threshold - last_close + courtage_log
-          } 
-        }    
+            sell_price = min(low_stop_loss_threshold, open)
+            sell_at(sell_price)
+            profit <- sell_price - last_close + courtage_log
+          } else {
+            profit <- close - last_close
+          }
+        }
       } else {
         if(up) {
           if (low < low_buy_threshold) {
-            buy_at(low_buy_threshold)
+            buy_price = min(open, low_buy_threshold)
+            buy_at(buy_price)
             profit <- courtage_log
-            low_stop_loss_threshold <- buy_point + log_stop_loss
             if (low < low_stop_loss_threshold) {
               sell_at(low_stop_loss_threshold)
               profit <- profit + low_stop_loss_threshold - buy_point + courtage_log
@@ -141,20 +190,20 @@ get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, same_day_trade_a
               profit <-
                 profit + high_sell_threshold - buy_point + courtage_log
             } else {
-              profit <- profit + close - buy_point
+              profit <- profit + close - buy_point + courtage_log
             }
           }
         } else {
           if (low < low_buy_threshold) {
-            buy_at(low_buy_threshold)
+            buy_price = min(open, low_buy_threshold)
+            buy_at(buy_price)
             profit <- courtage_log
-            low_stop_loss_threshold <- buy_point + log_stop_loss
             if (low < low_stop_loss_threshold) {
               sell_at(low_stop_loss_threshold)
               profit <-
                 profit + low_stop_loss_threshold - buy_point + courtage_log
             } else {
-              profit <- profit + close - buy_point
+              profit <- profit + close - buy_point + courtage_log
             }
           }
         }
@@ -166,8 +215,17 @@ get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, same_day_trade_a
       return(c(hold, profit, trade_times))
     }
 
+    if (type=='kalman') {
+      df <- kalman(df, HHt_val, GGt_val)
+    } else {
+      df <- ema(df, days, beta)
+    }
+    
+    if (is.null(df)) {
+      return(df)
+    }
+      
 
-    df <- kalman(df, HHt_val, GGt_val)
     result <- apply(df %>% mutate(row_id=row_number()) , 1, function(x) daily_trade(x))
     df$hold=t(result)[,1]
     df$trade_profit=t(result)[,2]
@@ -176,33 +234,55 @@ get_daily_profit <- function(par, df, verbose=TRUE, plot=FALSE, same_day_trade_a
     #return(cbind(df, df_result))
 }
                     
-result_summary <- function(df, stop_loss, courtage=COURTAGE, verbose=FALSE) {
+result_summary <- function(df, stop_loss, courtage=COURTAGE, verbose=FALSE, optimize=TRUE) {
   if (is.null(df)) {
     # invalid
     return(-1)
   }
   
-  if(verbose) {
-    summary <- df %>% filter(trade_profit != 0) %>% summarise(sum=sum(trade_profit), times=sum(trade_times), n=n())
-    avg_profit <- summary$sum / summary$n
+  if (optimize == FALSE) {
+    summary1 <- df %>% filter(trade_profit != 0 | hold == 1) %>% 
+      summarise(sum=sum(trade_profit), times=sum(trade_times), n=n())
+    avg_profit <- summary1$sum / summary1$n
     len <- dim(df)[1]
     avg_profit_no_model <- (df$mean[len] - df$mean[1])/len    
     sum_no_model <- df$mean[len] - df$mean[1]
-    print(summary)
+    if (verbose) {
+      print(summary1)
+    }
   }
 
-  
-  
-  summary2 <- df %>% mutate(id=row_number()) %>% mutate(range=cut(id, seq(0, max(id)+252, 252))) %>% group_by(range) %>% summarise(sum=sum(trade_profit)) 
+  summary2 <- df %>% mutate(id=row_number()) %>% mutate(range=cut(id, seq(0, max(id)+252, 252))) %>% 
+    filter(trade_profit != 0 | hold == 1) %>% 
+    group_by(range) %>% summarise(sum=sum(trade_profit), n=n())
+
   annual_mean <- mean(summary2$sum)
   annual_sd <- sd(summary2$sum)
+  annual_daily_mean <- mean(summary2$sum / summary2$n, na.rm=TRUE)
+  annual_daily_sd <- sd(summary2$sum / summary2$n, na.rm=TRUE)
   if (verbose) {
     print(summary2)
-    print(str_glue("avg_profit:{avg_profit}, nomodel_profit:{avg_profit_no_model}, nomodel_sum: {sum_no_model} sum:{summary$sum}, trade_times:{summary$times} courtage: {courtage}"))
+    print(str_glue("avg_profit:{avg_profit}, nomodel_profit:{avg_profit_no_model}, nomodel_sum: {sum_no_model} sum:{summary1$sum}, trade_times:{summary1$times} hold_days:{summary1$n}, courtage: {courtage}"))
     print(str_glue("annaul_mean:{annual_mean} annual_sd:{annual_sd}"))
+    print(str_glue("annual_daily_mean:{annual_daily_mean} annual_daily_sd:{annual_daily_sd}"))
   }
   #pulish with low stop_loss and longer holding days.
-  return(annual_mean - 0.5 * annual_sd + 0.2 * log(stop_loss))
+  #return(annual_mean - 0.5 * annual_sd + 0.2 * log(stop_loss))
+  if (optimize) {
+    if (is.na(annual_daily_mean) || is.na(annual_daily_sd)) {
+      # the model did nothing, punish.
+      annual_daily_mean = -0.001
+      annual_daily_sd = 0
+    }
+    return(annual_daily_mean - 0.5 * annual_daily_sd + 0.002 * log(stop_loss))
+  } else {
+    return(list(trade_times=summary1$times, hold_days=summary1$n, annual_daily_mean=annual_daily_mean, 
+        annual_daily_sd=annual_daily_sd, sum_no_model=sum_no_model, sum=summary1$sum, avg_profit_no_model=avg_profit_no_model,
+        avg_profit=avg_profit))
+    
+  }
+  
+
   
 }
 
@@ -221,28 +301,35 @@ left_merge_with_rowname <- function(x,y)
 
 get_sek_usd <- function() {
   sek_df <- data.frame(getSymbols('SEK=X',src='yahoo',auto.assign=FALSE))
-  rownames(sek_df) <- lapply(rownames(sek_df), function(x) str_replace_all(substring(x, 2), "\\.", '-'))
+  #rownames(sek_df) <- lapply(rownames(sek_df), function(x) str_replace_all(substring(x, 2), "\\.", '-'))
 
   sek_df$SEK.X.Mean <- (sek_df$SEK.X.High + sek_df$SEK.X.Close) / 2
   sek_df <- sek_df %>% select(SEK.X.Mean)
   return(sek_df)
 }
 
-                             
+data_filter <- function(df, start_date, end_date) {
+  df %>% filter(rownames(df) > start_date) %>% filter(rownames(df) < end_date)
+}
+                       
 g_sek_df = NULL
-data_prep <- function(stock_name, sek_df=NULL) {
+data_prep <- function(stock_name, sek_df=NULL, start_date='2007-01-01', end_date="2022-09-30") {
   if (is.null(sek_df)) {
     if (is.null(g_sek_df)) {
-      g_sek_df <<- get_sek_usd()
+      g_sek_df <<- get_sek_usd() %>% data_filter(start_date, end_date)
+      
     } 
     sek_df <- g_sek_df
   }
   df <- data.frame(getSymbols(stock_name,src='yahoo',auto.assign=FALSE))
+  df <- df %>% data_filter(start_date, end_date)
   df <- left_merge_with_rowname(df, sek_df)
   df$SEK.X.Mean <- na.approx(df$SEK.X.Mean)
   df[,1:4] <- df[,1:4] * df$SEK.X.Mean
   df <- data.frame(log(df)) %>% select(-last_col())
 
-  df <- df %>% set_colnames(c('open', 'high', 'low', 'close', 'volume', 'adjusted'))
+  df <- df %>% set_colnames(c('open', 'high', 'low', 'close', 'volume', 'adjusted')) %>% 
+    mutate(last_close=shift(close))
+  df$last_close[1] <- df$open[1]
   return(df)
 }
