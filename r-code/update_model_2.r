fb49161f74c24fba8ff0c91e65321d35586363c4
Ken <smartestrobotdai@gmail.com>
2022-11-02 14:36:36 +0100

    -

---
+++ r-code/update_model_2.r
@@ -0,0 +1,46 @@
+# go through models:
+library(rjson)
+source('util.r')
+dir <- '../models/model1'
+
+result <- fromJSON(file = str_glue("{dir}/desc.json"))
+securities <- result$securities
+securitie_states <- list()
+latest_last_data_date = '1900-01-01'
+
+for (security in securities) {
+  id <- security[['id']]
+  HHt <- security[['HHt']]
+  GGt <- security[['GGt']]
+  sell <- security[['sell']]
+  buy <- security[['buy']]
+  use_close <- as.numeric(security[['use-close']])
+  to_sek <- as.numeric(security[['to-sek']])
+  isNYSE <- as.numeric(security[['market']] == 'NYSE')
+  res <- data_prep(id, HHt=HHt, GGt=GGt, isNYSE=isNYSE,
+    predict=TRUE, use_close=use_close, to_sek=to_sek)
+
+  last_data_date <- res[1]
+  if (last_data_date > latest_last_data_date) {
+    latest_last_data_date = last_data_date
+  }
+
+  predict <- as.numeric(res[2])
+  buy_point <- predict + buy
+  sell_point <- predict + sell
+  predict <- exp(1)**predict
+  buy_point <- exp(1)**buy_point
+  sell_point <- exp(1)**sell_point
+  securitie_states <- append(securitie_states, list(list(id=id,
+    last_data_date=last_data_date, 
+    predict=predict, buy_point=buy_point, sell_point=sell_point)))
+}
+
+cur_time <- Sys.time()
+output <- list(last_update_time=as.character(cur_time), 
+  security_states=securitie_states)
+
+outfile <- str_glue("{dir}/state-{latest_last_data_date}.json")
+out = file(outfile, 'w')
+print(output)
+write(toJSON(output), out)
---
+++ server/server_v3.py
@@ -0,0 +1,205 @@
+import asyncio
+from email import message_from_string
+import json
+import json
+from os import path
+import time
+from datetime import timedelta
+from dateutil import tz
+import datetime
+from pandas.tseries.offsets import BDay
+import aiohttp
+from yticker_pb2 import yaticker
+import base64
+import random
+from websockets import serve
+import websockets
+import pandas as pd
+import numpy as np
+import logging
+
+security_state = []
+
+logging.basicConfig(format='%(asctime)s %(message)s', filename='./server.log', encoding='utf-8', level=logging.INFO)
+NYSE_HOLIDAYS = ['2022-11-24', '2022-12-26', '2023-01-02', '2023-01-16', 
+'2023-02-20', '2023-04-07', '2023-05-29', '2023-06-19']
+
+NYT = tz.gettz('America/New_York')
+
+
+ticks=[]
+client_websockets = []
+df_state_last_update_time = datetime.datetime.now()
+df_state_last_print_time = datetime.datetime.now()
+
+
+def get_last_workday_nyse(timestamp=None):
+  if timestamp is None:
+    timestamp = datetime.datetime.now(NYT)
+
+  candidate = timestamp - BDay(1)
+  if candidate.strftime("%Y-%m-%d") in NYSE_HOLIDAYS:
+    return (timestamp - BDay(2)).strftime("%Y-%m-%d") 
+  return candidate.strftime("%Y-%m-%d") 
+
+def get_today_nyse(timestamp):
+  if timestamp is None:
+    timestamp = datetime.datetime.now(NYT)
+
+  return timestamp.strftime("%Y-%m-%d")
+
+
+def validate_date(last_data_date, last_b_date, today_date):
+    # it could be today (i.e. at night)
+    return last_b_date == last_data_date or today_date == last_data_date
+
+def get_valid_securities():
+  security_dict = {}
+  f = open(get_state_file_name())
+  state = json.load(f)
+  timestamp = datetime.datetime.now(NYT)
+  last_b_date = get_last_workday_nyse(timestamp)
+  today_date = get_today_nyse(timestamp)
+  security_states = state['security_states']
+  return [s for s in security_states if validate_date(s['last_data_date'], last_b_date, today_date)]
+
+  
+def get_security_names():
+    f = open('../models/model1/desc.json')
+    desc = json.load(f)
+    return desc['securities']
+
+async def send_msg_to_all(id, point, operation):
+  global client_websockets
+  websockets_to_remove = []
+  obj_to_send = {"id": id, "point": point, "operation": operation}
+  str_to_send = json.dumps(obj_to_send)
+  for websocket in client_websockets:
+    try:
+      logging.info(f'send data to {websocket}')
+      await websocket.send(str_to_send)
+    except websockets.exceptions.ConnectionClosed:
+      logging.info("Client disconnected.  Do cleanup")
+      websockets_to_remove.append(websocket)
+      continue
+  for websocket in websockets_to_remove:
+    client_websockets.remove(websocket)
+
+
+
+def find_first_index(lst, condition):
+  filtered = [i for i, elem in enumerate(lst) if condition(elem)]
+  if len(filtered) == 0:
+    return None
+  else:
+    return filtered[0]
+
+def update_state(id, new_state, price):
+  global security_state
+  index = find_first_index(security_state, lambda e: e['id']==id)
+  if index is None:
+    logging.error(f'{id} not in security_list, please check')
+    return None    
+  cur_state = security_state[index]['state']
+  if cur_state != new_state:
+    logging.info(f'changed {id} from {cur_state} to {new_state} at {price}')
+    security_state[index]['state'] = new_state
+
+async def check_msg(data):
+  global security_state
+  my_yaticker = yaticker()
+  message = base64.b64decode(data)
+  my_yaticker.ParseFromString(message)
+  data = {
+      "id": my_yaticker.id,
+      "exchange": my_yaticker.exchange,
+      "quoteType": my_yaticker.quoteType,
+      "price": my_yaticker.price,
+      "timestamp": my_yaticker.time,
+      "marketHours": my_yaticker.marketHours,
+      "changePercent": my_yaticker.changePercent,
+      "dayVolume": my_yaticker.dayVolume,
+      "change": my_yaticker.change,
+      "priceHint": my_yaticker.priceHint
+  }
+  id = data['id']
+  index = find_first_index(security_state, lambda e: e['id']==id)
+
+  if index is None:
+    logging.info(f'{id} not in security_list, please check')
+    return None    
+
+  states = security_state[index]
+  buy_point = states['buy_point']
+  sell_point = states['sell_point']
+  price = data['price']
+  if price < buy_point:
+    update_state(id, 'buy', price)
+    await send_msg_to_all(id, buy_point, 'buy')
+  elif price > sell_point:
+    update_state(id, 'sell', price)
+    await send_msg_to_all(id, sell_point, 'sell')
+  else:
+    update_state(id, 'normal', price)
+
+async def send_request(name_list, ws):
+  symbol_list = dict()
+  symbol_list["subscribe"] = name_list
+  send_str = json.dumps(symbol_list)
+  await ws.send_str(send_str)
+  logging.info(f'data sent: {name_list}')
+
+def get_state_file_name():
+  last_workday_nyse = get_last_workday_nyse()
+  return f'../models/model1/state-{last_workday_nyse}.json'
+
+def validate_model_date():
+  return path.isfile(get_state_file_name())
+
+
+async def echo(websocket):
+  client_websockets.append(websocket)
+  async for message in websocket:
+    await websocket.send(message)
+
+async def init_server():
+  logging.info('initialize server')
+  async with serve(echo, None, 8766) as target:
+    await asyncio.Future()
+
+async def init_client():
+  # DELETE ME, TESTING
+  #name_list = ['BTC-USD', 'ETH-USD', 'USDT-USD']
+  id_list = [s['id'] for s in security_state]
+  async with aiohttp.ClientSession() as session:
+    #async with session.ws_connect("wss://streamer.finance.yahoo.com/") as ws:
+    async with session.ws_connect("wss://streamer.finance.yahoo.com/") as ws:
+      await send_request(id_list, ws)
+      async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+          await check_msg(msg.data)
+          if msg.data == 'close cmd':
+            await ws.close()
+            break
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+          break
+
+
+async def main():
+  global security_state
+  logging.info('validate model state')
+  valid = validate_model_date()
+  if not valid:
+    logging.info('Validating file failed, exiting...')
+    exit(1)
+  security_state = get_valid_securities()
+  if len(security_state) < 1:
+    logging.info('no valid id found')
+    exit(1)
+  security_state = [{**s, 'state':'init'} for s in security_state]
+  print(security_state)
+  logging.info(security_state)
+  asyncio.ensure_future(init_client())
+  await init_server()
+
+asyncio.run(main())
